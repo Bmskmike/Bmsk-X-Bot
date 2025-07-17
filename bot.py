96a303ef543299c82fcc9c672f2ce0432f9c36ba
BMSK <111078510+Bmskmike@users.noreply.github.com>
2025-07-17 20:40:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,29 @@ import schedule
 import time
 import os
 from dotenv import load_dotenv
+import os
+
+api_key = os.getenv("TWITTER_API_KEY")
+api_secret = os.getenv("TWITTER_API_SECRET")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_SECRET")
+
+print("API Key:", api_key)
+print("API Secret:", api_secret)
+print("Access Token:", access_token)
+print("Access Secret:", access_token_secret)
+
+import tweepy
+
+auth = tweepy.OAuth1UserHandler(
+    api_key,
+    api_secret,
+    access_token,
+    access_token_secret
+)
+
+api = tweepy.API(auth)
+print("Bot is running successfully.")
 
 load_dotenv()
  # Load credentials
