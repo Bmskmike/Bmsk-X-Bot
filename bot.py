30ea71b76b409c07beb752f9222341cf2425ceb7
BMSK <111078510+Bmskmike@users.noreply.github.com>
2025-07-17 18:00:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-# Load credentials
+ Load credentials
 api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
 # Authenticate
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
